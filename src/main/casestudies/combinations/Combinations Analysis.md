0def4ef197768534b7d9433dfe9f06b294f0dd2f
Jiacheng Yang <39355540+haijunme@users.noreply.github.com>
2022-03-27 15:08:48 -0400

    Added a V3 version code to `Combinations`

--- src/main/casestudies/combinations/Combinations Analysis.md
+++ src/main/casestudies/combinations/Combinations Analysis.md
@@ -10,3 +10,10 @@ Recursion with too much array creating, resizing and copying.
 By adding backtracking to reuse already allocated ArrayLists
 ### Why much faster?
 Less ArrayList operations = significantly less operations
+### Combinations V3
+![img_2.png](img_2.png)
+### What's so interesting?
+The only significant difference between V2 and V3 is that, I replaced one 
+ArrayList.add() and one ArrayList.remove(X.size()-1) with one 
+LinkedList.add() and one LinkedList.removeLast(); however, V3 just simply performs worse,
+which is another proof that LinkedList sucks 
--- src/main/casestudies/combinations/CombinationsV3.java
+++ src/main/casestudies/combinations/CombinationsV3.java
@@ -1,35 +1,39 @@
 package main.casestudies.combinations;
 
+import sun.awt.image.ImageWatched;
+
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
 
-public class CombinationsV2 {
-    private final List<List<Integer>> answer = new ArrayList<>();
+public class CombinationsV3 {
+    private final List<List<Integer>> answer = new LinkedList<>();
     int n;
     int k;
 
-    private void recur(int number, List<Integer> combination) {
+    private void recur(int number, LinkedList<Integer> combination) {
         if (combination.size() == k) {
-            answer.add(new ArrayList<>(combination));
+            answer.add(new LinkedList<>(combination));
             return;
         }
 
         for (int i = number; i <= n; i++) {
             combination.add(i);
             recur(i + 1, combination);
-            combination.remove(combination.size() - 1);
+            combination.removeLast();
         }
     }
 
     public List<List<Integer>> combine(int n, int k) {
         this.n = n;
         this.k = k;
-        recur(1,  new ArrayList<>());
+        recur(1, new LinkedList<>());
         return answer;
     }
 
     public static void main(String[] args) {
-        CombinationsV2 solution = new CombinationsV2();
+        CombinationsV3 solution = new CombinationsV3();
         List<List<Integer>> answer = solution.combine(4, 3);
         for (List<Integer> combination : answer) {
             System.out.println(combination.toString());
