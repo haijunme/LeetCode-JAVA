753315ece6d589181766e58936818ab9ab6d6cc3
Jiacheng Yang <39355540+haijunme@users.noreply.github.com>
2022-03-29 14:35:11 -0400

    Uploaded more solutions 2022-03-29 2:35pm

---
+++ src/main/quickpractices/dynamicprogramming/NthTribonacchiNumber.java
@@ -0,0 +1,22 @@
+package main.quickpractices.dynamicprogramming;
+
+public class NthTribonacchiNumber {
+    public int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+
+        int[] memo = new int[n+1];
+        memo[0] = 0;
+        memo[1] = 1;
+        memo[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            memo[i] = memo[i-1] + memo[i-2] + memo[i-3];
+        }
+        return memo[n];
+    }
+
+    public static void main(String[] args) {
+        NthTribonacchiNumber nthTribonacchiNumber = new NthTribonacchiNumber();
+        System.out.println(nthTribonacchiNumber.tribonacci(25));
+    }
+}
