00d4b4a150268a4b788189a2de0509b69808b661
Jiacheng Yang <39355540+haijunme@users.noreply.github.com>
2022-03-30 23:13:25 -0400

    upload more solutions 2022-03-30 11:11pm

---
+++ src/main/quickpractices/BoundaryOfBinaryTree.java
@@ -0,0 +1,55 @@
+package main.quickpractices;
+
+import main.shared.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoundaryOfBinaryTree {
+    private final List<Integer> boundary = new ArrayList<>();
+    private final List<Integer> leftBoundary = new ArrayList<>();
+    private final List<Integer> leaves = new ArrayList<>();
+    private final List<Integer> rightBoundary = new ArrayList<>();
+
+    private void addLeaves(TreeNode node) {
+        if(node == null) return;
+        if(node.left == null && node.right == null) leaves.add(node.val);
+        addLeaves(node.left);
+        addLeaves(node.right);
+    }
+
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        boundary.add(root.val);
+        TreeNode node = root.left;
+        while (node != null) {
+            if (node.left != null || node.right != null) {
+                leftBoundary.add(node.val);
+            }  else {
+                leaves.add(node.val);
+            }
+            if (node.left != null) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+
+        node = root.right;
+        while (node != null) {
+            if (node.left != null || node.right != null) {
+                rightBoundary.add(0, node.val);
+            }  else {
+                leaves.add(node.val);
+            }
+            if (node.right != null) {
+                node = node.right;
+            } else {
+                node = node.left;
+            }
+        }
+        boundary.addAll(leftBoundary);
+        boundary.addAll(leaves);
+        boundary.addAll(rightBoundary);
+        return boundary;
+    }
+}
