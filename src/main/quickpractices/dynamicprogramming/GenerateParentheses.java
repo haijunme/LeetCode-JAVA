8e9788345e110d42c55e2043c21857bd0ff79bae
Jiacheng Yang <39355540+haijunme@users.noreply.github.com>
2022-03-30 21:28:48 -0400

    upload more solutions 2022-03-20 9:28pm

---
+++ src/main/quickpractices/dynamicprogramming/GenerateParentheses.java
@@ -0,0 +1,14 @@
+package main.quickpractices.dynamicprogramming;
+
+import java.util.List;
+
+public class GenerateParentheses {
+
+    public List<String> generateParenthesis(int n) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
