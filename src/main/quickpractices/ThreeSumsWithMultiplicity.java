28fb2afd2d9bd99cb57ae4149f34a15544dca38e
Jiacheng Yang <39355540+haijunme@users.noreply.github.com>
2022-04-06 10:45:22 -0400

    Added ZigZagConversion 2022-04-06 10:45pm

---
+++ src/main/quickpractices/ThreeSumsWithMultiplicity.java
@@ -0,0 +1,10 @@
+package main.quickpractices;
+
+//public class ThreeSumsWithMultiplicity {
+//
+//    public int threeSumMulti(int[] arr, int target) {
+//        for (int i = 0; i < arr.length; i++) {
+//
+//        }
+//    }
+//}
