4bb29797eb60a8abae91849da7b743af12a86604
Jiacheng Yang <39355540+haijunme@users.noreply.github.com>
2022-05-04 16:09:19 -0400

    uploaded more solutions 2022-05-04 4:09pm

--- src/main/quickpractices/ReverseLinkedList.java
+++ src/main/quickpractices/ReverseLinkedList.java
@@ -6,10 +6,9 @@ public class ReverseLinkedList {
     public ListNode reverseList(ListNode head) {
         ListNode previous = null;
         ListNode current = head;
-        ListNode next;
 
         while (current != null) {
-            next = current.next;
+            ListNode next = current.next;
             current.next = previous;
             previous = current;
             current = next;
