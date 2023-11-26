2e0ce521c9d52dafd48748b1a81e8a8f8d759094
haijunme <yjc961020@gmail.com>
2023-11-26 11:31:34 -0500

    Add Q1727

---
+++ src/main/prep2024/weekly/Q1727_LargestSubmatrixWithRearrangements.java
@@ -0,0 +1,14 @@
+package main.prep2024.weekly;
+
+public class Q1727_LargestSubmatrixWithRearrangements {
+  public int largestSubmatrix(int[][] matrix) {
+    // Use BFS to get the distances of all nuts relative to the squirrel
+
+    // Use BFS to get the distances of all nuts relative to the tree
+
+    // Locate the first nut. Result = D(S, N1) + D(T, N1) for the smallest possibility.
+
+    // Calculate the back and forth for all other nuts. Result = 2*D(T, N2) + 2*D(T,N3) ...
+    return 0;
+  }
+}
--- src/main/prep2024/weekly/Q346_MovingAverageFromDataStream.java
+++ src/main/prep2024/weekly/Q346_MovingAverageFromDataStream.java
@@ -1,4 +1,40 @@
 package main.prep2024.weekly;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Q346_MovingAverageFromDataStream {
+  public static class MovingAverage {
+    private final Deque<Integer> slidingWindow;
+    private final int maxSize;
+    private int currentSum;
+
+    public MovingAverage(int size) {
+      slidingWindow = new ArrayDeque<>(size);
+      maxSize = size;
+      currentSum = 0;
+    }
+
+    public double next(int val) {
+      if (slidingWindow.size() >= maxSize) {
+        currentSum -= slidingWindow.removeFirst();
+      }
+
+      slidingWindow.addLast(val);
+      currentSum += val;
+
+      return ((double) currentSum)/slidingWindow.size();
+    }
+  }
+
+  private static void testHelper(MovingAverage solution, int... input) {
+    for (int j : input) {
+      System.out.println(solution.next(j));
+    }
+  }
+
+  public static void main(String[] args) {
+    var solution = new MovingAverage(3);
+    testHelper(solution, 1, 10, 3, 5);
+  }
 }
